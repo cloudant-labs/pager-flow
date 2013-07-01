757b7d4ac93f6625f72855e415f3e0e602253b2f
suhkang <suhkang@msu.edu>
2013-07-01 09:50:03 -0700

    Update pagerflow.py

--- pagerflow.py
+++ pagerflow.py
@@ -207,7 +207,6 @@ def upload(json_doc):
     headers = {"content-type": "application/json"}
     url = DB_URL+'/_bulk_docs' if 'docs' in json_doc else DB_URL
     resp = requests.post(url, auth=(DB_ID, DB_PASSWD), data=json.dumps(json_doc), headers=headers)
-    print resp
     return resp.status_code in [201, 202]
 
 
